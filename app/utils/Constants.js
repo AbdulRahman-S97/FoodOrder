371822e578fd003a9bff819186b538a59cdd08fd
Abdul Rahman <cracyabdul@gmail.com>
2021-10-13 17:08:19 +0530

    constants file added

---
+++ app/utils/Constants.js
@@ -0,0 +1,42 @@
+/*************************************************
+ * FoodOrderApp
+ * @exports
+ * Constants.js
+ * Created by Abdul Rahman on 13/10/2021
+ *************************************************/
+
+'use strict';
+
+/**
+ * Returns all the constants used in the application
+ * Separate constants according to the category and usage
+ */
+module.exports = {
+  ACTIONS: {
+    //Cart
+    CART_COUNT_UPDATE: 'CART_COUNT_UPDATE',
+    RESET_CART_TO_INITIAL: 'RESET_CART_TO_INITIAL',
+  },
+  COLOR: {
+    THEME_COLOR: '#12183F',
+    THEME_SECONDARY: '#7D90C9',
+    WHITE_COLOR: '#FFFFFF',
+    BLACK_COLOR: 'black',
+    DARK_MODE: '#121212',
+  },
+
+  FONT_SIZE: {
+    MONSTER_X: 62,
+    MONSTER: 60,
+    MEGA: 40,
+    XXXL: 26,
+    XXL: 24,
+    XL: 20,
+    L: 18,
+    M: 16,
+    REGULAR: 14,
+    SR: 13,
+    S: 12,
+    XS: 10,
+  },
+};
